2c5862d308401ce46c41c554b78fb60d7bdd3101
josePC <jose.csousa22@gmail.com>
2023-04-02 15:09:41 -0300

    feat(): Correcoes na geracao de regex

--- RegexGenerator.py
+++ RegexGenerator.py
@@ -1,10 +1,12 @@
 import random
 import string
-from PIL import Image, ImageDraw, ImageFont
+import re
+
 
 # Gera uma sequência aleatória de caracteres alfabéticos
 def generate_random_string(length):
     return ''.join(random.choices(string.ascii_letters, k=length))
+
 
 # Gera uma expressão regular no formato x|y ou x|y*
 def generate_random_regex():
@@ -15,16 +17,33 @@ def generate_random_regex():
         regex = f'{x}|{y}'
     else:
         regex = f'{x}|{y}*'
+
     return regex
 
-# Função para criar uma imagem de gradiente de botão
-def create_button_gradient(size, colors, text_color):
-    img = Image.new('RGBA', size, color=colors[0])
-    draw = ImageDraw.Draw(img)
-    draw.rectangle((0, 0, size[0] // 2, size[1]), fill=colors[0], outline=None)
-    draw.rectangle((size[0] // 2, 0, size[0], size[1]), fill=colors[1], outline=None)
-    font_size = int(size[1] * 0.6)
-    font = ImageFont.truetype('arial.ttf', font_size)
-    text_width, text_height = draw.textsize('Ok', font=font)
-    draw.text(((size[0] - text_width) // 2, (size[1] - text_height) // 2), 'Ok', fill=text_color, font=font)
-    return img.tobytes()
+
+def generate_two_regex():
+    letras = list(string.ascii_letters)
+    regexGerado = []
+
+    while len(regexGerado) != 6:
+        regex1 = generate_random_regex()
+        regex2 = generate_random_regex()
+        regex3 = generate_random_regex()
+        regex4 = generate_random_regex()
+
+        for letra in letras:
+            x1 = re.fullmatch(regex3, letra) and re.fullmatch(regex1, letra)
+            x2 = re.fullmatch(regex3, letra) and re.fullmatch(regex2, letra)
+            x3 = re.fullmatch(regex4, letra) and re.fullmatch(regex1, letra)
+            x4 = re.fullmatch(regex4, letra) and re.fullmatch(regex2, letra)
+
+            if x3 and x1 and x2 and x4:
+                regexGerado.append(regex1)
+                regexGerado.append(regex2)
+                regexGerado.append(f"resposta 1 = {letra}")
+                regexGerado.append(regex3)
+                regexGerado.append(regex4)
+                regexGerado.append(f"resposta 2 = {letra}")
+                break
+
+    return regexGerado
--- main.py
+++ main.py
@@ -3,25 +3,23 @@ import PySimpleGUI as sg
 import RegexGenerator as rg
 
 # Define o layout da janela
-randomRegex = []
-for k in range(0, 4):
-    randomRegex.append(rg.generate_random_regex())
 
+randomRegex = rg.generate_two_regex()
+
+print(randomRegex)
 r1 = randomRegex[0]
 r2 = randomRegex[1]
-r3 = randomRegex[2]
-r4 = randomRegex[3]
+r3 = randomRegex[3]
+r4 = randomRegex[4]
 
 # Define o layout do formulário
 layout = [
-    [sg.Text('Regex crossword 2x2', font=('Helvetica', 18), justification='center', size=(30,1))],
-    [sg.Text('  ', size=(3, 1)), sg.Text(r1, size=(3, 1), font=('Helvetica', 16)), sg.Text(r2, size=(12, 1), font=('Helvetica', 16))],
-    [sg.Text(r3, font=('Helvetica', 16)), sg.Input(size=(4, 1), key='B1', font=('Helvetica', 16)), sg.Input(size=(4, 1), key='B2', font=('Helvetica', 16))],
-    [sg.Text(r4, font=('Helvetica', 16)), sg.Input(size=(4, 1), key='B3', font=('Helvetica', 16)), sg.Input(size=(4, 1), key='B4', font=('Helvetica', 16))],
-    [sg.Button('Ok', font=('Helvetica', 16), size=(4, 1), button_color=('white', '#007f5f'))]
+    [sg.Text('Regex crossword 2x2', font=('Helvetica', 36), justification='center', size=(40,1))],
+    [sg.Text(' ', size=(3, 1)), sg.Text(r1, size=(10, 1), font=('Helvetica', 28), justification='center'), sg.Text(r2, size=(28, 1), font=('Helvetica', 28), justification='center')],
+    [sg.Text(r3, font=('Helvetica', 28), justification='center'), sg.Input(size=(10, 1), key='B1', font=('Helvetica', 28), justification='center', pad=(10,10)), sg.Input(size=(10, 1), key='B2', font=('Helvetica', 28), justification='center', pad=(10,10))],
+    [sg.Text(r4, font=('Helvetica', 28), justification='center'), sg.Input(size=(10, 1), key='B3', font=('Helvetica', 28), justification='center', pad=(10,10)), sg.Input(size=(10, 1), key='B4', font=('Helvetica', 28), justification='center', pad=(10,10))],
+    [sg.Button('Ok', font=('Helvetica', 28), size=(10, 1), button_color=('white', '#007f5f'), pad=((200, 0), 40))]
 ]
-
-
 # Cria a janela com o layout definido
 window = sg.Window('Jogo da matriz 2x2', layout, size=(700, 700))
 
@@ -31,8 +29,8 @@ contador = 0
 # Gera matriz 2x2
 matriz = [[' ' for _ in range(2)] for _ in range(2)]
 
-regexLinha = [r1, r2]
-regexColuna = [r3, r4]
+regexColuna = [r1, r2]
+regexLinha = [r3, r4]
 
 letrasDigitasB1 = []
 letrasDigitasB2 = []
@@ -52,27 +50,27 @@ while True:
         linha = 0
         coluna = 0
         letra = values['B1'].strip()
-        letrasDigitasB1.append(values['B1'].lower())
+        letrasDigitasB1.append(values['B1'])
 
-    if values['B2'].strip() != "" and values['B2'].lower() not in letrasDigitasB2:
+    if values['B2'].strip() != "" and values['B2'] not in letrasDigitasB2:
         linha = 0
         coluna = 1
         letra = values['B2'].strip()
-        letrasDigitasB2.append(values['B2'].lower())
+        letrasDigitasB2.append(values['B2'])
 
-    if values['B3'].strip() != "" and values['B3'].lower() not in letrasDigitasB3:
+    if values['B3'].strip() != "" and values['B3'] not in letrasDigitasB3:
         linha = 1
         coluna = 0
         letra = values['B3'].strip()
-        letrasDigitasB3.append(values['B3'].lower())
+        letrasDigitasB3.append(values['B3'])
 
-    if values['B4'].lower() != "" and values['B4'].lower() not in letrasDigitasB4:
+    if values['B4'].strip() != "" and values['B4'] not in letrasDigitasB4:
         linha = 1
         coluna = 1
-        letra = values['B4'].lower()
-        letrasDigitasB4.append(values['B4'].lower())
+        letra = values['B4'].strip()
+        letrasDigitasB4.append(values['B4'])
 
-    matriz[linha][coluna] = letra
+    matriz[linha][coluna] = letra.strip()
 
     regex1 = regexLinha[linha].strip()
     regex2 = regexColuna[coluna].strip()
